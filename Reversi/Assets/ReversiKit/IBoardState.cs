a0bc6c321638b70e139ee99013ef42eef30cd8bd
dodikk <dodikk88.reg@gmail.com>
2015-12-17 19:42:05 +0200

    Added "board state" interface

---
+++ Reversi/Assets/ReversiKit/IBoardState.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ReversiKit
+{
+	public interface IBoardState
+	{
+		bool IsCellFree(ICellCoordinates position);
+		bool IsCellTakenByBlack(ICellCoordinates position);
+		bool IsCellTakenByWhite(ICellCoordinates position);
+	}
+}
+
