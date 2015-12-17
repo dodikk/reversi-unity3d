52ed746959a3ad025162b9dda7a69d8563b20a62
dodikk <dodikk88.reg@gmail.com>
2015-12-17 19:39:11 +0200

    Added turn structure

---
+++ Reversi/Assets/ReversiKit/IReversiTurn.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ReversiKit
+{
+	public interface IReversiTurn
+	{
+		ICellCoordinates position { get; }
+		IEnumerable<ICellCoordinates> positionsOfFlippedItems { get; }
+	}
+}
+
