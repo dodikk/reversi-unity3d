a2449e1e900bbc7fa53918f29150a0d73696a546
dodikk <dodikk88.reg@gmail.com>
2015-12-17 21:56:44 +0200

    [turn calculator] Added stub class

---
+++ Reversi/ReversiKit/ReversiKit/ReversiKit/Impl/TurnCalculator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ReversiKit
+{
+	public class TurnCalculator : ITurnCalculator
+	{
+		public TurnCalculator()
+		{
+		}
+			
+		#region ITurnCalculator
+		public IEnumerable<IReversiTurn> GetValidTurnsForBoard(IBoardState board)
+		{
+			throw new NotImplementedException();
+		}
+		#endregion
+	}
+}
+
