e5bfa806fe553ed2c5d324e41830f891abc8123e
dodikk <dodikk88.reg@gmail.com>
2015-12-17 20:20:15 +0200

    Added coordinates ==> text tests

--- Reversi/ReversiKit/ReversiKit/ReversiKitTest/CoordinatesTest.cs
+++ Reversi/ReversiKit/ReversiKit/ReversiKitTest/CoordinatesTest.cs
@@ -7,13 +7,15 @@ namespace ReversiKitTest
 	[TestFixture]
 	public class CoordinatesTest
 	{
+
+		#region Text To Point
 		[Test]
 		public void TestValidTextToField()
 		{
 			var received = BoardCoordinatesConverter.CellNameToCoordinates ("C5");
 
-			Assert.AreEqual(received.Row, 4);
-			Assert.AreEqual(received.Column, 2);
+			Assert.AreEqual(4, received.Row);
+			Assert.AreEqual(2, received.Column);
 		}
 
 		[Test]
@@ -39,7 +41,7 @@ namespace ReversiKitTest
 		}
 
 		[Test]
-		public void TestLargeColumnCausesException()
+		public void TestLargeColumnInNameCausesException()
 		{
 			TestDelegate failure = delegate() 
 			{
@@ -59,6 +61,70 @@ namespace ReversiKitTest
 
 			Assert.Catch(failure, "invalid column name");
 		}
+		#endregion
+
+
+		#region Point to Text
+		[Test]
+		public void TestValidFieldToText()
+		{
+			var position = new CellCoordinates (4, 2);
+			string received = BoardCoordinatesConverter.CoordinatesToCellName (position);
+
+			Assert.AreEqual("C5", received);
+		}
+
+		[Test]
+		public void TestNegativeColumnIndexCausesException()
+		{
+			var position = new CellCoordinates(3, -1);
+
+			TestDelegate failure = delegate() 
+			{
+				BoardCoordinatesConverter.CoordinatesToCellName (position);
+			};
+
+			Assert.Catch(failure, "invalid column name");
+		}
+
+		[Test]
+		public void TestNegativeRowCausesException()
+		{
+			var position = new CellCoordinates(-1, 0);
+
+			TestDelegate failure = delegate() 
+			{
+				BoardCoordinatesConverter.CoordinatesToCellName (position);
+			};
+
+			Assert.Catch(failure, "invalid row name");
+		}
+
+		[Test]
+		public void TestLargeRowCausesException()
+		{
+			var position = new CellCoordinates(9, 0);
+			TestDelegate failure = delegate() 
+			{
+				BoardCoordinatesConverter.CoordinatesToCellName (position);
+			};
+
+			Assert.Catch(failure, "invalid row name");
+		}
+
+		[Test]
+		public void TestLargeColumnCausesException()
+		{
+			var position = new CellCoordinates(0, 9);
+			TestDelegate failure = delegate() 
+			{
+				BoardCoordinatesConverter.CoordinatesToCellName (position);
+			};
+
+			Assert.Catch(failure, "invalid row name");
+		}
+		#endregion
+
 	}
 }
 
