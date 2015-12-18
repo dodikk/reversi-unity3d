15b8c4603a133904fc1d9391fee0e6e42c6522b0
dodikk <dodikk88.reg@gmail.com>
2015-12-18 15:48:48 +0200

    [calc] Added test for direction flips

--- Reversi/ReversiKit/ReversiKit/ReversiKitTest/TurnCalculatorTest.cs
+++ Reversi/ReversiKit/ReversiKit/ReversiKitTest/TurnCalculatorTest.cs
@@ -25,7 +25,7 @@ namespace ReversiKitTest
 				this._initialBoard.IsTurnOfBlackPlayer = true;
 			}
 
-			this._sut = new TurnCalculator(this._initialBoard);
+			this._sut = new TurnCalculator();
 		}
 
 		[TearDown]
@@ -45,7 +45,7 @@ namespace ReversiKitTest
 		// 3 x x x T x x x x
 		// 2 x x x x x x x x
 		// 1 x x x x x x x x
-		// A B C D E F G H
+		//   A B C D E F G H
 		[Test]
 		public void TestTurnsForInitialState()
 		{
@@ -133,6 +133,198 @@ namespace ReversiKitTest
 		}
 	
 
+        // 8 x x x x x x x x
+        // 7 x x x x x x x x
+        // 6 x x x x x x x x
+        // 5 x x x x x x x x
+        // 4 x x x x x x x x
+        // 3 x x x x x x x x
+        // 2 x x x x x x x x
+        // 1 x x x x x x x x
+        //   A B C D E F G H
+        [Test]
+        public void TestTurnsResultIsNullForEmptyBoard()
+        {
+            var board = new MatrixBoard();
+            var sut = new TurnCalculator();
+
+            var turns = sut.GetValidTurnsForBoard(board);
+            Assert.IsNull(turns);
+        }
+
+
+        // 8 W x x W x x x W
+        // 7 x x x B x x B x
+        // 6 x x x B x B x x
+        // 5 x x x B B x x x
+        // 4 W B B T B B B W
+        // 3 x x B B x x x x
+        // 2 x B x B x x x x
+        // 1 W x x W x x x W
+        //   A B C D E F G H
+        [Test]
+        public void TestMultipleDirections()
+        {
+            var sut = new TurnCalculator();
+            var board = new MatrixBoard();
+            {
+                board.IsTurnOfBlackPlayer = false;
+                board.TryConsumeNamedCellByWhitePlayer("A1");
+                board.TryConsumeNamedCellByWhitePlayer("H1");
+                board.TryConsumeNamedCellByWhitePlayer("A8");
+                board.TryConsumeNamedCellByWhitePlayer("H8");
+                board.TryConsumeNamedCellByWhitePlayer("A4");
+                board.TryConsumeNamedCellByWhitePlayer("H4");
+                board.TryConsumeNamedCellByWhitePlayer("D1");
+                board.TryConsumeNamedCellByWhitePlayer("D8");
+
+
+                // Diagonal
+                board.TryConsumeNamedCellByBlackPlayer("B2");
+                board.TryConsumeNamedCellByBlackPlayer("C3");
+                board.TryConsumeNamedCellByBlackPlayer("E5");
+                board.TryConsumeNamedCellByBlackPlayer("F6");
+                board.TryConsumeNamedCellByBlackPlayer("G7");
+
+                // Horizontal
+                board.TryConsumeNamedCellByBlackPlayer("B4");
+                board.TryConsumeNamedCellByBlackPlayer("C4");
+                board.TryConsumeNamedCellByBlackPlayer("E4");
+                board.TryConsumeNamedCellByBlackPlayer("F4");
+                board.TryConsumeNamedCellByBlackPlayer("G4");
+
+                // Vertical
+                board.TryConsumeNamedCellByBlackPlayer("D2");
+                board.TryConsumeNamedCellByBlackPlayer("D3");
+                board.TryConsumeNamedCellByBlackPlayer("D5");
+                board.TryConsumeNamedCellByBlackPlayer("D6");
+                board.TryConsumeNamedCellByBlackPlayer("D7");
+            }
+
+
+            var result = sut.GetValidTurnsForBoard(board);
+            Assert.IsNotNull(result);
+            Assert.AreEqual(1, result.Count());
+
+            IReversiTurn turn = result.First();
+            string strPosition = BoardCoordinatesConverter.CoordinatesToCellName(turn.Position);
+            Assert.AreEqual("D4", strPosition);
+
+            Assert.AreEqual(15, turn.PositionsOfFlippedItems.Count());
+
+            var sortedFlips = 
+                turn.PositionsOfFlippedItems.OrderBy(c =>
+                {
+                    return BoardCoordinatesConverter.CoordinatesToCellName(c);
+                });
+            string strSortedFlips = BoardCoordinatesConverter.PrintCoordinates(sortedFlips);
+            string expectedFlips = "B2; B4; C3; C4; D2; D3; D5; D6; D7; E4; E5; F4; F6; G4; G7";
+
+            Assert.AreEqual(expectedFlips, strSortedFlips);
+        }
+
+        // 8 B x x B x x x B
+        // 7 x x x B x x B x
+        // 6 x x x B x B x x
+        // 5 x x x B B x x x
+        // 4 B B B T B B B B
+        // 3 x x B B x x x x
+        // 2 x B x B x x x x
+        // 1 B x x B x x x B
+        //   A B C D E F G H
+        [Test]
+        public void TestNoFlipsIfBoardEndReached()
+        {
+            var sut = new TurnCalculator();
+            var board = new MatrixBoard();
+            {
+                board.IsTurnOfBlackPlayer = false;
+                board.TryConsumeNamedCellByBlackPlayer("A1");
+                board.TryConsumeNamedCellByBlackPlayer("H1");
+                board.TryConsumeNamedCellByBlackPlayer("A8");
+                board.TryConsumeNamedCellByBlackPlayer("H8");
+                board.TryConsumeNamedCellByBlackPlayer("A4");
+                board.TryConsumeNamedCellByBlackPlayer("H4");
+                board.TryConsumeNamedCellByBlackPlayer("D1");
+                board.TryConsumeNamedCellByBlackPlayer("D8");
+
+
+                // Diagonal
+                board.TryConsumeNamedCellByBlackPlayer("B2");
+                board.TryConsumeNamedCellByBlackPlayer("C3");
+                board.TryConsumeNamedCellByBlackPlayer("E5");
+                board.TryConsumeNamedCellByBlackPlayer("F6");
+                board.TryConsumeNamedCellByBlackPlayer("G7");
+
+                // Horizontal
+                board.TryConsumeNamedCellByBlackPlayer("B4");
+                board.TryConsumeNamedCellByBlackPlayer("C4");
+                board.TryConsumeNamedCellByBlackPlayer("E4");
+                board.TryConsumeNamedCellByBlackPlayer("F4");
+                board.TryConsumeNamedCellByBlackPlayer("G4");
+
+                // Vertical
+                board.TryConsumeNamedCellByBlackPlayer("D2");
+                board.TryConsumeNamedCellByBlackPlayer("D3");
+                board.TryConsumeNamedCellByBlackPlayer("D5");
+                board.TryConsumeNamedCellByBlackPlayer("D6");
+                board.TryConsumeNamedCellByBlackPlayer("D7");
+            }
+
+
+            var result = sut.GetValidTurnsForBoard(board);
+            Assert.IsNull(result);
+        }
+
+
+        // 8 W x x x x x x x
+        // 7 x B x x x x x x
+        // 6 x x B x x x x x
+        // 5 x x x B x x x x
+        // 4 x x x x B x x x
+        // 3 x x x x x T x x
+        // 2 x x x x x x B x
+        // 1 x x x x x x x W
+        //   A B C D E F G H
+        [Test]
+        public void TestReverseDiagonalFlips()
+        {
+            var sut = new TurnCalculator();
+            var board = new MatrixBoard();
+            {
+                board.IsTurnOfBlackPlayer = false;
+
+                board.TryConsumeNamedCellByWhitePlayer("A8");
+                board.TryConsumeNamedCellByWhitePlayer("H1");
+
+                board.TryConsumeNamedCellByBlackPlayer("B7");
+                board.TryConsumeNamedCellByBlackPlayer("C6");
+                board.TryConsumeNamedCellByBlackPlayer("D5");
+                board.TryConsumeNamedCellByBlackPlayer("E4");
+                board.TryConsumeNamedCellByBlackPlayer("G2");
+            }
+
+            var result = sut.GetValidTurnsForBoard(board);
+            Assert.IsNotNull(result);
+            Assert.AreEqual(1, result.Count());
+
+            IReversiTurn turn = result.First();
+            string strPosition = BoardCoordinatesConverter.CoordinatesToCellName(turn.Position);
+            Assert.AreEqual("F3", strPosition);
+
+            Assert.AreEqual(5, turn.PositionsOfFlippedItems.Count());
+
+            var sortedFlips = 
+                turn.PositionsOfFlippedItems.OrderBy(c =>
+                {
+                    return BoardCoordinatesConverter.CoordinatesToCellName(c);
+                });
+            string strSortedFlips = BoardCoordinatesConverter.PrintCoordinates(sortedFlips);
+            string expectedFlips = "B7; C6; D5; E4; G2";
+
+            Assert.AreEqual(expectedFlips, strSortedFlips);
+        }
+
 		// 8 x x x x x x x x
 		// 7 x x x x x x x x
 		// 6 x x x x x x x x
@@ -141,7 +333,7 @@ namespace ReversiKitTest
 		// 3 x x x x x x x x
 		// 2 x x x x x x x x
 		// 1 x x x x x x x x
-		// A B C D E F G H
+		//   A B C D E F G H
 
 
 	}
