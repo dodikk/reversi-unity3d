00615a72924b42874ec9a7585af5c6699c1398f9
dodikk <dodikk88.reg@gmail.com>
2015-12-18 02:45:51 +0200

    [cell] Implemented comparator and Clone() methods

--- Reversi/Assets/ReversiKit/CellCoordinates.cs
+++ Reversi/Assets/ReversiKit/CellCoordinates.cs
@@ -2,7 +2,7 @@
 
 namespace ReversiKit
 {
-	public class CellCoordinates : ICellCoordinates
+    public class CellCoordinates : ICellCoordinates, ICloneable
 	{
 		public CellCoordinates (int row, int column)
 		{
@@ -10,8 +10,41 @@ namespace ReversiKit
 			this.Column = column;
 		}
 
+
+        public object Clone()
+        {
+            CellCoordinates result = new CellCoordinates(this.Row, this.Column);
+            return result;
+        }
+
+		#region Equality
+		public override bool Equals(object other)
+		{
+			CellCoordinates otherCell = other as CellCoordinates;
+			if (null == otherCell)
+			{
+				return false;
+			}
+
+			bool rowEqual = (this.Row == otherCell.Row);
+			bool colEqual = (this.Column == otherCell.Column);
+
+			return rowEqual && colEqual;
+		}
+
+		public override  int GetHashCode()
+		{
+			int rowHash = this.Row.GetHashCode();
+			int columnHash = this.Column.GetHashCode();
+
+			return (10 * rowHash) + columnHash;
+		}
+		#endregion
+
+		#region ICellCoordinates
 		public int Row {get; set;}
 		public int Column {get; set;}
+		#endregion
 	}
 }
 
