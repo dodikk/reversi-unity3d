87424d3015b9a575d91a64ea2697b2111cfff4a6
dodikk <dodikk88.reg@gmail.com>
2015-12-19 22:21:56 +0200

    Added builder to unity project

---
+++ Reversi/Assets/ReversiKit/TurnSelectorBuilder.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ReversiKit
+{
+    public class TurnSelectorBuilder
+    {
+        private TurnSelectorBuilder()
+        {
+        }
+
+        public static ITurnSelector CreateCornerAndGreedyTurnSelector()
+        {
+            var cornerSelector = new CornerTurnSelector();
+            var borderSelector = new BorderTurnSelector();
+            var greedySelector = new GreedyTurnSelector();
+
+            ITurnSelector[] selectors = new ITurnSelector[3] 
+            { 
+                cornerSelector,  
+                borderSelector,
+                greedySelector
+            };
+
+
+            CompositeTurnSelector result = new CompositeTurnSelector(selectors);
+            return result;
+        }
+
+    }
+}
+
