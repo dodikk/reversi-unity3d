fb71a711c2bcb7ac49ca3d1090e03d7033e76650
dodikk <dodikk88.reg@gmail.com>
2015-12-19 21:49:40 +0200

    Removed a todo statement

--- Reversi/Assets/ReversiKit/ITurnValidator.cs
+++ Reversi/Assets/ReversiKit/ITurnValidator.cs
@@ -4,7 +4,6 @@ namespace ReversiKit
 {
 	public interface ITurnValidator
 	{
-		// TODO : maybe IEnumerable<IreversiTurn> should be used
 		bool IsValidPositionForTurnOnBoard(ICellCoordinates turnPosition, IBoardState board);
 	}
 }
