505871b85700f3dba65cebd9d1fd6c83076a0a9a
dodikk <dodikk88.reg@gmail.com>
2015-12-19 15:30:48 +0200

    Added turn selector strategies

---
+++ Reversi/Assets/ReversiKit/GreedyTurnSelector.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ReversiKit
+{
+    public class GreedyTurnSelector : ITurnSelector
+    {
+        public IReversiTurn SelectBestTurnOnBoard(
+            IEnumerable<IReversiTurn> validTurns, 
+            IBoardState board)
+        {
+            IReversiTurn result = 
+                validTurns.OrderByDescending(t => t.PositionsOfFlippedItems.Count())
+                          .First();
+
+            return result;
+        }
+    }
+}
+
---
+++ Reversi/Assets/ReversiKit/RandomTurnSelector.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+namespace ReversiKit
+{
+    public class RandomTurnSelector : ITurnSelector
+    {
+        public IReversiTurn SelectBestTurnOnBoard(
+            IEnumerable<IReversiTurn> validTurns, 
+            IBoardState board)
+        {
+            Random rand = new Random();
+            int index = rand.Next(0, validTurns.Count());
+        
+            IReversiTurn result = validTurns.ElementAt(index);
+            return result;
+        }
+    }
+}
+
