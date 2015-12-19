c2ed9f14c280b090c7c102d0eac8440e930623f7
dodikk <dodikk88.reg@gmail.com>
2015-12-19 21:19:28 +0200

    Added turn validator stub impl

---
+++ Reversi/ReversiKit/ReversiKit/ReversiKit/Impl/SearchInSetTurnValidator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ReversiKit
+{
+    public class SearchInSetTurnValidator : ITurnValidator
+    {
+        public SearchInSetTurnValidator(IEnumerable<IReversiTurn> validTurns)
+        {
+            this._validTurns = validTurns;
+        }
+
+        public bool IsValidPositionForTurnOnBoard(
+            ICellCoordinates turnPosition, 
+            IBoardState board)
+        {
+            throw new NotImplementedException();
+        }
+
+        private IEnumerable<IReversiTurn> _validTurns;
+    }
+}
+
